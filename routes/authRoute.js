c24eaa435249a3584bb32fd9a3d56144089789d8
Rauch Rodrigues <rauch.rodrigues@koseapp.com>
2024-03-24 21:19:41 +0530

    added login auth

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -2,6 +2,7 @@ import express from "express";
 import { User } from "../models/userModel.js";
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 const router = express.Router();
 
@@ -24,6 +25,36 @@ router.post("/signup", async (req, res, next) => {
     };
     const user = await User.create(securedUser);
     res.status(201).json({ message: "User created succesfully!", body: user });
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post("/login", async (req, res, next) => {
+  try {
+    const validUser = await User.findOne({ email: req.body.email });
+    if (!validUser) {
+      throw new Error("Email does not exist");
+    }
+
+    const validPassword = bcryptjs.compareSync(
+      req.body.password,
+      validUser.password
+    );
+
+    if (!validPassword) {
+      throw new Error("Wrong Credentials");
+    }
+
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+
+    const {password, ...rest} = validUser._doc;
+
+
+    res
+      .cookie("access_token", token, { httpOnly: true })
+      .status(201)
+      .json({ message: "User logged in  succesfully!", body: rest });
   } catch (error) {
     next(error);
   }
